830187893b063ad0cff95b972861a48185b761c2
unknownssassss <mahdi.khaleghi1234@gmail.com>
2017-07-31 08:58:05 +0430

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,4 +1,7 @@
-while true; do
-   ./skyteam.sh
-   sleep 2
-  done
+#!/bin/bash
+COUNTER=1
+while(true) do
+./skyteam.sh
+curl "https://api.telegram.org/bot418563293:AAECqX5pBxpyBwDXdxZGIV_7zPAvY28ZZC0/sendmessage" -F "chat_id=441247469" -F "text=#NEWCRASH-#skybot-Reloaded-${COUNTER}-times"
+let COUNTER=COUNTER+1 
+done
